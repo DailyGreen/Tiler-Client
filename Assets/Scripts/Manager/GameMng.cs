fc977d99f1a8e6f040fe0287466050fea61fb56f
kyechan99 <kyechan99@gmail.com>
2021-01-04 19:27:11 +0900

    [추가] 인게임 뼈대 인터페이스

--- Assets/Scripts/Manager/GameMng.cs
+++ Assets/Scripts/Manager/GameMng.cs
@@ -10,6 +10,12 @@ public class GameMng : MonoBehaviour
     private static GameMng _Instance = null;
 
     //public bool wantToBuilt = false;
+    [SerializeField]
+    UnityEngine.UI.Text objectNameTxt;          // 선택 오브젝트 이름
+    [SerializeField]
+    UnityEngine.UI.Text objectDetailTxt;        // 선택 오브젝트 디테일
+    [SerializeField]
+    UnityEngine.UI.Text hpText;                 // HP 디테일
 
     public static GameMng I
     {
@@ -38,5 +44,22 @@ public class GameMng : MonoBehaviour
     public void RemoveDelegate(CountTurn Method)
     {
         this.countDel -= Method;
-    }  
+    }
+
+    /**
+     * @brief 오브젝트를 클릭했을때
+     * @param 으로 클릭한 오브젝트가 뭔지 알려줘야됨
+     */
+    public void clickObject(/* 뭘 클릭했는지 넘겨주기 */)
+    {
+        // 만약 지형이면
+        /// 지형 에 대해 알려주기
+
+        // 캐릭터면
+        /// 캐릭터 에 대해 알려주기
+
+        objectNameTxt.text = "오브젝트 이름";
+        objectDetailTxt.text = "오브젝트 설명";
+        hpText.text = "오브젝트 설명";
+    }
 }

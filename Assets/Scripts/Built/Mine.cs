8448c433743ef5764f66b30a4a007464db1848b2
swkang99 <kseongwoo99@gmail.com>
2021-01-05 15:16:44 +0900

    광산 코드 정리 및 게임오브젝트 크기 키움

--- Assets/Scripts/Built/Mine.cs
+++ Assets/Scripts/Built/Mine.cs
@@ -10,17 +10,32 @@ public class Mine : Built
     // Start is called before the first frame update
     void Start()
     {
+        Init();
+    }
+
+    /// <summary>
+    /// 초기화 함수
+    /// </summary>
+    void Init ()
+    {
         _name = "광산";
         _desc = "골드를 캘 수 있다";
         _hp = 10;
         _cost = 0;
-
         stack = 0;
         making = 5;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        MakingGold();
+    }
+
+    /// <summary>
+    /// 골드를 제조함
+    /// </summary>
+    void MakingGold()
     {
         if (Input.GetKey("g"))
             stack += making;

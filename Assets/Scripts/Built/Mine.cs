d794d98e214091f02126353ad4219ab19e8c0be7
swkang99 <kseongwoo99@gmail.com>
2021-01-05 14:53:32 +0900

    create 'mine' GameObject

---
+++ Assets/Scripts/Built/Mine.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : Built
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        _hp = 10;
+        _cost = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

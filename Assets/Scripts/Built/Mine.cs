295aece1654f0489ef35777be8b2e14b0f98eeea
Sun <taeyang9300@daum.net>
2021-01-05 19:32:05 +0900

    [추가]플레이어이동, 행동 인터페이스, 건설

---
+++ Assets/Scripts/Built/Mine.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : Built
+{
+    public int making;  // °ñµå »ý»ê·®
+    public static int cost = 1;   // °Ç¼³ ºñ¿ë
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _name = "±¤»ê";
+        _desc = "°ñµå¸¦ Ä¶ ¼ö ÀÖ´Ù";
+        _hp = 10;
+        making = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MakingGold();
+        if (Input.GetMouseButtonDown(0))
+            Building(this.gameObject);
+    }
+
+    /**
+     * @brief °ñµå »ý»ê
+     */
+    void MakingGold()
+    {
+        if (Input.GetKeyDown("g"))
+            GameMng.I.addGold(making);
+    }
+}
--- Assets/Scripts/Unit/Worker.cs
+++ Assets/Scripts/Unit/Worker.cs
@@ -6,14 +6,64 @@ public class Worker : Unit
 {
     void Start()
     {
-        _hp = 0;
+        _name = "일꾼";
+        _desc = "일해라 노예야";
         _cost = 0;
-        _damage = 0;
-        _code = 1;
+        _activity.Add(ACTIVITY.BUILD_MINE);
+        _activity.Add(ACTIVITY.BUILD_FARM);
+        _activity.Add(ACTIVITY.BUILD_ATTACK_BUILDING);
+        _activity.Add(ACTIVITY.BUILD_CREATE_UNIT_BUILDING);
+        _activity.Add(ACTIVITY.BUILD_SHIELD_BUILDING);
+        _activity.Add(ACTIVITY.BUILD_UPGRADE_BUILDING);
     }
 
     void Update()
     {
-        CharClickMove(this.gameObject);
+        GameMng.I.UnitClickMove(this.gameObject, this);
+    }
+
+    public static void buildMine()
+    {
+        Debug.Log("광산 생성");
+    }
+
+    public static void buildFarm()
+    {
+        Debug.Log("농장 생성");
+    }
+
+    public static void buildAttackBuilding()
+    {
+        Debug.Log("터렛 생성");
+    }
+
+    public static void buildCreateUnitBuilding()
+    {
+        Debug.Log("유닛 건물 생성");
+    }
+
+    public static void buildShieldBuilding()
+    {
+        Debug.Log("방어 건물 생성");
+    }
+
+    public static void buildUpgradeBuilding()
+    {
+        Debug.Log("강화 건물 생성");
     }
 }
+
+/* 
+
+광산 짓기
+골드 창고 짓기
+농장 짓기
+식량 창고 짓기
+공격 건물 짓기 
+유닛 생성 건물 짓기
+방어 건물 짓기
+강화 건물 짓기
+건물 수리
+
+
+*/

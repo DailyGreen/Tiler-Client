7ddf89d0d46e3b0746e2a8640f36969cecf2edd0
swkang99 <kseongwoo99@gmail.com>
2021-01-05 15:02:02 +0900

    골드 생산 기능 추가

--- Assets/Scripts/Built/Mine.cs
+++ Assets/Scripts/Built/Mine.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class Mine : Built
 {
+    public int stack;   // º¸À¯ °ñµå·®
+    public int making;  // »ý»ê °ñµå·®
+
     // Start is called before the first frame update
     void Start()
     {
+        _name = "±¤»ê";
+        _desc = "°ñµå¸¦ Ä¶ ¼ö ÀÖ´Ù";
         _hp = 10;
         _cost = 0;
+
+        stack = 0;
+        making = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("g"))
+            stack += making;
     }
 }
